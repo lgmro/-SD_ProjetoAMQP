95c954fc652d5a75602f6f62afb74d7b47e0b060
Lucas Gabriel <marinholgabriel@gmail.com>
2022-11-23 21:54:25 -0300

    Mudando mensagens

--- lavanderia_matriz.py
+++ lavanderia_matriz.py
@@ -20,8 +20,10 @@ def main():
   	=========================================================
   	''')
 
+    mensagens = ["O cliente solicitou o uso de uma maquina na lavanderia 01", "A maquina da lavanderia 03 esta em uso", "A maquina da lavanderia 04 esta livre", "O cliente esta solicitando o uso de uma maquina na lavanderia 05", "A maquina da lavanderia 01 nao esta mais em uso"]
+
     for i in range(1,6):
-        mensagem = f"Cliente solicitou o uso de uma maquina na lavanderia 0{i}"
+        mensagem = mensagens[i-1]
         mensagem_matriz = f"Mensagem 0{i}: {mensagem}"
         channel.basic_publish(exchange="messagens_matriz", routing_key="", body=mensagem_matriz)
         print(f"Enviado a mensagem 0{i} -> {mensagem}")
